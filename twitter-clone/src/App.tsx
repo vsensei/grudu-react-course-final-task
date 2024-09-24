95d198901f5b4eb501d3c1f4186723fb53f86d0a
Myagistu, Nikita <afanasevn@ae.com>
2024-09-24 05:40:36 +0300

    used the new NewPost component, updated the styles

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import NewPost from './components/NewPost';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <div className='main'>
+        <NewPost />
+      </div>
     </div>
   );
 }
