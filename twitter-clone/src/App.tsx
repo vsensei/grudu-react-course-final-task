c242c2c8fe7cf2ccd348a4f39f234375ddcb8811
Myagistu, Nikita <afanasevn@ae.com>
2024-09-26 00:40:57 +0300

    added navigation

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,16 +1,41 @@
 import './App.css';
-import Header from './components/Header';
 import NewPost from './components/NewPost';
 import Feed from './components/Feed';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Root from './pages/Root';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        {
+          path: '/',
+          element: (
+            <>
+              <NewPost />
+              <Feed />
+            </>
+          ),
+        },
+        {
+          path: '/signin',
+          element: <SignIn />,
+        },
+        {
+          path: '/signup',
+          element: <SignUp />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <div className='App'>
-      <Header />
-      <div className='main'>
-        <NewPost />
-        <Feed />
-      </div>
+      <RouterProvider router={router} />
     </div>
   );
 }
